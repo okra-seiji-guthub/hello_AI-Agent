158ce8cbd713663de1b5bb4ee1922086e7113846
s.okura <ookura.seiji@outlook.com>
2026-08-10 13:59:57 +0000

    edit task and output

--- src/HelloAIAgent.py
+++ src/HelloAIAgent.py
@@ -1,7 +1,5 @@
 import agents
 
-
-
 llm = agents.LLM(
     # model="ollama/qwen2.5:7b",           # ollama/<model_name> format
     model="ollama/qwen2.5:3b-instruct",  # ollama/<model_name> format
@@ -9,13 +7,9 @@ llm = agents.LLM(
 )
 
 def main_proc():
+
     task = """最新のPython AI Agent開発用フレームワークの動向について調査し、レポートを作成してください。
-    キーワード：
-    Python AI Agent開発用フレームワーク (CrewAI, AutoGen, LangChain, etc.)
-    最新の動向
-    レポート
-    """
-    output = """Markdown形式でレポートを作成してください。
+    
     1. 最新のPython AI Agent開発用フレームワークの概要
     2. 主要なPython AI Agent開発用フレームワークの比較
     3. 主要なPython AI Agent開発用フレームワークの比較表
@@ -24,8 +18,9 @@ def main_proc():
         2. 利用可能なツール
         3. 難易度(5段階評価)
         4. 特徴と用途
-    4. 今後の展望と課題
-    """
+    4. 今後の展望と課題"""
+    
+    output = """Markdown形式でレポートを作成してください。"""
 
     # result = agents.Reseacher_JP(verbose=True).build_agent(task, output).kickoff()
     result = agents.Reseacher(llm=llm, lang="Japanese", verbose=True).build_agent(task, output).kickoff()
@@ -34,3 +29,4 @@ def main_proc():
     
 if __name__ == "__main__":
     main_proc()
+
